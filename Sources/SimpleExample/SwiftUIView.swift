ac5489f1ee6de582fab730c7e83b4266dc47111b
Ambas <ambaschobsanti@gmail.com>
2023-08-29 13:24:55 +0700

    Rename view

--- Sources/SimpleExample/SwiftUIView.swift
+++ Sources/SimpleExample/SwiftUIView.swift
@@ -9,10 +9,49 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SimpleExample(title: "Sample", items: [
+            .init(title: "Sample 1", view: SampleContent1()),
+            .init(title: "Sample 2", view: SampleContent1())
+        ])
+    }
+}
+
+struct SampleContent1: View {
+    var body: some View {
+        Text("Sample 1")
     }
 }
 
 #Preview {
     SwiftUIView()
+}
+
+
+public struct SimpleExampleView: View {
+    
+    public struct Item {
+        let title: String
+        let view: AnyView
+        public init(title: String, view: some View) {
+            self.title = title
+            self.view = AnyView(view)
+        }
+    }
+    
+    let items: [Item]
+    let title: String
+    
+    public init(title: String, items: [Item]) {
+        self.title = title
+        self.items = items
+    }
+    
+    public var body: some View {
+        NavigationView {
+            List(items, id: \.title) { item in
+                NavigationLink(destination: item.view, label: { Text(item.title) })
+            }
+            .navigationTitle(title)
+        }
+    }
 }

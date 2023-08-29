9b05ba5b5bcfff6c8fa3d7be33d16ead00265f07
Ambas <ambaschobsanti@gmail.com>
2023-08-29 13:21:49 +0700

    initial commit

---
+++ Sources/SimpleExample/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Ambas Chobsanti on 29/8/2566 BE.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
